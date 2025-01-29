0525669b06d72ea1c5d2095ec0b7b247cf305432
gomezzzer <gomezgeorge132@gmail.com>
2025-01-29 14:30:34 -0600

    set_caption

--- code/main.py
+++ code/main.py
@@ -4,4 +4,5 @@ class Game:
     def __init__(self):
         pygame.init()
         self.display_surfface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT)) 
+        pygame.display.set_caption('Survivor') 
 
