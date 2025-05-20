5168ab0bcdf5fdcc6874210df3bf5a718355d3b7
gomezzzer <gomezgeorge132@gmail.com>
2025-05-20 18:40:27 -0500

    game class

--- code/main.py
+++ code/main.py
@@ -3,6 +3,23 @@ from settings import *
 class Game: 
     def __init__(self):
         pygame.init()
-        self.display_surfface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT)) 
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT)) 
         pygame.display.set_caption('Survivor') 
+        self.clock = pygame.time.Clock()
+        self.running = True 
+        
+    def run(self):
+        while self.running:
+            
+            dt = self.clock.tick() / 1000 
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            
+            pygame.display.flip()
+        pygame.quit()       
+                    
 
+game = Game()
+game.run() 
